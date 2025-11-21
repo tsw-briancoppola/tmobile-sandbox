262bb291004eab89c2ebc31409f5ea119b5d5a1d
Brian Coppola <BrianW.Coppola2@T-Mobile.com>
2025-11-20 16:10:06 -0800

    Added basic functionality to high school list

---
+++ high-school-list/local/data/data.js
@@ -0,0 +1,37 @@
+const schoolData = [
+  {
+    id: 0,
+    name: "A. Crawford Mosley H.S.",
+    city: "Lynn Haven",
+    state: "FL",
+    website: "https://mosleyfootball.com/",
+  },
+  {
+    id: 1,
+    name: "Abbeville H.S.",
+    city: "Abbeville",
+    state: "SC",
+    website: "https://ahs.acsdsc.org/o/abbeville-high-school",
+  },
+  {
+    id: 2,
+    name: "Bosqueville H.S.",
+    city: "Waco",
+    state: "TX",
+    website: "https://hs.bosquevilleisd.org/",
+  },
+  {
+    id: 3,
+    name: "Calexico H.S.",
+    city: "Calexico",
+    state: "CA",
+    website: "https://chs.cusdk12.org/",
+  },
+  {
+    id: 4,
+    name: "Davenport H.S.",
+    city: "Davenport",
+    state: "WA",
+    website: "https://www.davenportsd.org/",
+  },
+];
--- high-school-list/local/script.js
+++ high-school-list/local/script.js
@@ -1,1 +1,76 @@
+const searchBox = document.querySelector(".tsw-hs-list-container .search input");
+const list = document.querySelector(".tsw-hs-list");
 
+// =-=-=-=-=-=-=-=-=-=-=-=
+// Populate list with data
+// =-=-=-=-=-=-=-=-=-=-=-=
+
+const createRow = (school) => {
+  const dataRow = document.createElement("div");
+  dataRow.classList.add("tsw-hs-list__row");
+  const dataProperties = Object.keys(school);
+
+  dataProperties.forEach((property) => {
+    if (property !== "id") {
+      const dataRowCell = document.createElement("div");
+      const dataClassName = `tsw-hs-list__row--${property}`;
+      dataRowCell.classList.add(dataClassName);
+      dataRowCell.textContent = school[property];
+      dataRow.appendChild(dataRowCell);
+    }
+  });
+
+  return dataRow;
+};
+
+const addData = (data) => {
+  // Creating fragment for better performance
+  const fragment = document.createDocumentFragment();
+
+  data.forEach((school) => {
+    const row = createRow(school);
+    fragment.appendChild(row);
+  });
+
+  list.appendChild(fragment);
+};
+
+const clearData = () => {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+};
+
+// =-=-=-=-=-
+// Search box
+// =-=-=-=-=-
+
+const filterData = (data, value) => {
+  const filtered = data.filter((row) => {
+    // Return result only if search value is contained in school's name or city
+    const rowValues = [row.name, row.city];
+    return rowValues.some((d) => d.toLowerCase().includes(value.toLowerCase()));
+  });
+
+  return filtered;
+};
+
+const updateData = (value) => {
+  const filteredData = filterData(schoolData, value);
+  clearData();
+  addData(filteredData);
+};
+
+searchBox.addEventListener("input", () => {
+  updateData(searchBox.value);
+});
+
+// =-=-=-=
+// On load
+// =-=-=-=
+
+const init = () => {
+  addData(schoolData);
+};
+
+init();

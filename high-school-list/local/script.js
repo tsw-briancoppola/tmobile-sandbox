4fcc06ca82e69bae8fb3b78089a6b8d4db7f82f4
Brian Coppola <BrianW.Coppola2@T-Mobile.com>
2025-12-09 13:47:55 -0800

    Added simple pagination for high school list

--- high-school-list/local/script.js
+++ high-school-list/local/script.js
@@ -1,15 +1,24 @@
-const searchBox = document.querySelector(
-  ".tsw-hs-list-container .search input"
-);
+const searchBox = document.querySelector("#tsw-hs-list-search");
+const searchClearButton = document.querySelector(".tsw-hs-list-search-clear");
 const list = document.querySelector(".tsw-hs-list");
+const listEmpty = document.querySelector(".tsw-hs-list-empty");
+const prevButton = document.querySelector(".tsw-pagination-prev");
+const nextButton = document.querySelector(".tsw-pagination-next");
+
+let currentPage = 0;
+const itemsPerPage = 15;
 
 // =-=-=-=-=-=-=-=-=-
 // Pull data from API
 // =-=-=-=-=-=-=-=-=-
 
 const API_URL = "https://playground.mockoon.com/contacts";
+let apiData = [];
+let currentDataSet = []; // Subset of all data that passes search requirements
 
 const fetchApiData = async () => {
+  // const url = `${API_URL}?page=${currentPage}&limit=${itemsPerPage}`;
+
   try {
     const response = await fetch(API_URL);
     if (!response.ok) {
@@ -17,6 +26,8 @@ const fetchApiData = async () => {
     }
 
     const data = await response.json();
+    console.log(response.headers);
+
     return data;
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -51,22 +62,33 @@ const createRow = (school) => {
   return dataRow;
 };
 
+const clearData = () => {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+};
+
 const renderData = (data) => {
+  clearData();
+
   // Creating fragment for better performance
   const fragment = document.createDocumentFragment();
 
-  data.forEach((school) => {
+  data.length === 0
+    ? listEmpty.classList.add("active")
+    : listEmpty.classList.remove("active");
+
+  // Get current page slice of page data
+  const startItem = currentPage * itemsPerPage;
+  const endItem = startItem + itemsPerPage;
+  const pageData = data.slice(startItem, endItem);
+
+  pageData.forEach((school) => {
     const row = createRow(school);
     fragment.appendChild(row);
   });
 
   list.appendChild(fragment);
-};
-
-const clearData = () => {
-  while (list.firstChild) {
-    list.removeChild(list.firstChild);
-  }
 };
 
 // =-=-=-=-=-=-=-=-=
@@ -91,8 +113,8 @@ const debounce = (func, delay = 200) => {
 
 const filterData = (data, value) => {
   const filtered = data.filter((row) => {
-    // Return true only if search value is contained in school's name or city
-    const rowValues = [row.name, row.city];
+    // Return true only if search value is contained in school's name, city, or state
+    const rowValues = [row.name, row.city, row.state];
     return rowValues.some((d) => d.toLowerCase().includes(value.toLowerCase()));
   });
 
@@ -100,24 +122,63 @@ const filterData = (data, value) => {
 };
 
 const updateRenderedData = (value) => {
-  const filteredData = filterData(apiData, value);
-  clearData();
-  renderData(filteredData);
+  currentPage = 0; // Reset page to 0 when search terms change
+  currentDataSet = filterData(apiData, value);
+  renderData(currentDataSet);
 };
 
 // Set debounce to 200ms
 const debouncedUpdate = debounce(updateRenderedData, 200);
 
+// Toggle display of search box clear button
+const searchClearButtonToggle = () => {
+  searchBox.value
+    ? (searchClearButton.style.display = "block")
+    : (searchClearButton.style.display = "none");
+};
+
+// Event listener for search box
 searchBox.addEventListener("input", () => {
   debouncedUpdate(searchBox.value);
+  searchClearButtonToggle();
+});
+
+// Clear search box when clear button is clicked
+searchClearButton.addEventListener("click", () => {
+  searchBox.value = "";
+  searchBox.focus();
+  searchClearButtonToggle();
+  updateRenderedData("");
+});
+
+// =-=-=-=-=-
+// Pagination
+// =-=-=-=-=-
+
+const updatePaginationButtons = () => {
+  return null;
+};
+
+prevButton.addEventListener("click", () => {
+  if (currentPage > 0) {
+    currentPage--;
+    renderData(currentDataSet);
+    updatePaginationButtons(currentDataSet.length); // Update button states
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  const totalPages = Math.ceil(currentDataSet.length / itemsPerPage);
+  if (currentPage < totalPages - 1) {
+    currentPage++;
+    renderData(currentDataSet);
+    updatePaginationButtons(currentDataSet.length); // Update button states
+  }
 });
 
 // =-=-=-=
 // On load
 // =-=-=-=
-
-// API data variable
-let apiData = [];
 
 const init = async () => {
   const data = await fetchApiData();
@@ -131,9 +192,11 @@ const init = async () => {
       return { name, city, state, website };
     });
 
-    //
-    apiData = newDataArray.filter((_, index) => index < 20);
-    renderData(apiData);
+    // Set data size
+    apiData = newDataArray.filter((_, index) => index < 40);
+    currentDataSet = [...apiData];
+    // Render data
+    renderData(currentDataSet);
   }
 };
 

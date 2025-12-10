5f50ebba8a9c502ee409c5da98436d9b0ca05157
Brian Coppola <BrianW.Coppola2@T-Mobile.com>
2025-12-10 15:30:12 -0800

    Added pagination with page numbers to hs list

--- high-school-list/local/script.js
+++ high-school-list/local/script.js
@@ -2,11 +2,15 @@ const searchBox = document.querySelector("#tsw-hs-list-search");
 const searchClearButton = document.querySelector(".tsw-hs-list-search-clear");
 const list = document.querySelector(".tsw-hs-list");
 const listEmpty = document.querySelector(".tsw-hs-list-empty");
+const paginationContainer = document.querySelector(".tsw-hs-list-pagination");
 const prevButton = document.querySelector(".tsw-pagination-prev");
 const nextButton = document.querySelector(".tsw-pagination-next");
+const paginationNumbersContainer = document.querySelector(
+  ".tsw-pagination-numbers"
+);
 
 let currentPage = 0;
-const itemsPerPage = 15;
+const itemsPerPage = 10;
 
 // =-=-=-=-=-=-=-=-=-
 // Pull data from API
@@ -20,17 +24,113 @@ const fetchApiData = async () => {
   // const url = `${API_URL}?page=${currentPage}&limit=${itemsPerPage}`;
 
   try {
-    const response = await fetch(API_URL);
+    const response = await fetch("./data/mock_data.json");
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     const data = await response.json();
-    console.log(response.headers);
-
     return data;
   } catch (error) {
     console.error("Error fetching data:", error);
+    return null;
+  }
+};
+
+// =-=-=-=-=-
+// Pagination
+// =-=-=-=-=-
+
+const updatePaginationButtons = () => {
+  const totalPages = Math.ceil(currentDataSet.length / itemsPerPage);
+
+  prevButton.disabled = currentPage === 0;
+  nextButton.disabled = currentPage >= totalPages - 1;
+};
+
+prevButton.addEventListener("click", () => {
+  if (currentPage > 0) {
+    currentPage--;
+    renderData(currentDataSet);
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  const totalPages = Math.ceil(currentDataSet.length / itemsPerPage);
+  if (currentPage < totalPages - 1) {
+    currentPage++;
+    renderData(currentDataSet);
+  }
+});
+
+// =-=-=-=-=-=-=-=-=-
+// Pagination numbers
+// =-=-=-=-=-=-=-=-=-
+
+const createPaginationNumber = (index, text) => {
+  const button = document.createElement("button");
+  button.textContent = text;
+  button.classList.add("tsw-pagination-number");
+
+  if (index === currentPage) {
+    button.classList.add("active");
+    button.disabled = true; // Current page shouldn't be clickable
+  } else {
+    button.addEventListener("click", () => {
+      currentPage = index;
+      renderData(currentDataSet);
+    });
+  }
+
+  paginationNumbersContainer.appendChild(button);
+};
+
+const createEllipsis = () => {
+  const span = document.createElement("span");
+  span.textContent = "...";
+  span.classList.add("tsw-pagination-ellipsis");
+  paginationNumbersContainer.appendChild(span);
+};
+
+const renderPaginationNumbers = () => {
+  paginationNumbersContainer.innerHTML = "";
+
+  const totalPages = Math.ceil(currentDataSet.length / itemsPerPage);
+
+  // Hide pagination if there are less than 2 pages
+  const shouldHidePagination = totalPages <= 1;
+  paginationContainer.classList.toggle("is-hidden", shouldHidePagination);
+
+  if (shouldHidePagination) {
+    return;
+  }
+
+  const indexForThirdToLastPage = totalPages - 3;
+  const isNearEnd = currentPage >= indexForThirdToLastPage;
+
+  // Near the end
+  if (isNearEnd) {
+    let startIndex = Math.max(0, totalPages - 3);
+
+    for (let i = startIndex; i < totalPages; i++) {
+      createPaginationNumber(i, (i + 1).toString());
+    }
+  }
+  // Middle pages
+  else {
+    const startPageIdx = currentPage;
+    const endPageIdx = currentPage + 2;
+
+    for (let i = startPageIdx; i <= endPageIdx; i++) {
+      if (i >= totalPages - 1) break;
+      createPaginationNumber(i, (i + 1).toString());
+    }
+
+    if (endPageIdx < totalPages - 1) {
+      createEllipsis();
+      // Add the very last page index
+      createPaginationNumber(totalPages - 1, totalPages.toString());
+    }
   }
 };
 
@@ -41,23 +141,15 @@ const fetchApiData = async () => {
 const createRow = (school) => {
   const dataRow = document.createElement("div");
   dataRow.classList.add("tsw-hs-list__row");
-  const dataProperties = Object.keys(school);
 
-  dataProperties.forEach((property) => {
-    const dataRowCell = document.createElement("div");
-    const dataClassName = `tsw-hs-list__row--${property}`;
-    dataRowCell.classList.add(dataClassName);
-    if (property === "website") {
-      const dataLink = document.createElement("a");
-      dataLink.setAttribute("href", school[property]);
-      dataLink.setAttribute("target", "_blank");
-      dataLink.textContent = "School website";
-      dataRowCell.appendChild(dataLink);
-    } else {
-      dataRowCell.textContent = school[property];
-    }
-    dataRow.appendChild(dataRowCell);
-  });
+  dataRow.innerHTML = `
+    <div class="tsw-hs-list__row--name">${school.name}</div>
+    <div class="tsw-hs-list__row--city">${school.city}</div>
+    <div class="tsw-hs-list__row--state">${school.state}</div>
+    <div class="tsw-hs-list__row--website">
+      <a href="${school.website}" target="_blank">School website</a>
+    </div>
+  `;
 
   return dataRow;
 };
@@ -74,9 +166,8 @@ const renderData = (data) => {
   // Creating fragment for better performance
   const fragment = document.createDocumentFragment();
 
-  data.length === 0
-    ? listEmpty.classList.add("active")
-    : listEmpty.classList.remove("active");
+  // Toggle 'No results' notice based on data length
+  listEmpty.classList.toggle("active", data.length === 0);
 
   // Get current page slice of page data
   const startItem = currentPage * itemsPerPage;
@@ -89,6 +180,8 @@ const renderData = (data) => {
   });
 
   list.appendChild(fragment);
+  updatePaginationButtons();
+  renderPaginationNumbers();
 };
 
 // =-=-=-=-=-=-=-=-=
@@ -151,29 +244,15 @@ searchClearButton.addEventListener("click", () => {
   updateRenderedData("");
 });
 
-// =-=-=-=-=-
-// Pagination
-// =-=-=-=-=-
+// =-=-=-=-=-=-
+// Toggle theme
+// =-=-=-=-=-=-
 
-const updatePaginationButtons = () => {
-  return null;
-};
+const listContainer = document.querySelector(".tsw-hs-list-container");
+const themeToggleButton = document.querySelector(".tsw-hs-list-theme-toggle");
 
-prevButton.addEventListener("click", () => {
-  if (currentPage > 0) {
-    currentPage--;
-    renderData(currentDataSet);
-    updatePaginationButtons(currentDataSet.length); // Update button states
-  }
-});
-
-nextButton.addEventListener("click", () => {
-  const totalPages = Math.ceil(currentDataSet.length / itemsPerPage);
-  if (currentPage < totalPages - 1) {
-    currentPage++;
-    renderData(currentDataSet);
-    updatePaginationButtons(currentDataSet.length); // Update button states
-  }
+themeToggleButton.addEventListener("click", () => {
+  listContainer.classList.toggle("dark-theme");
 });
 
 // =-=-=-=
@@ -181,23 +260,34 @@ nextButton.addEventListener("click", () => {
 // =-=-=-=
 
 const init = async () => {
-  const data = await fetchApiData();
-  if (data) {
-    // Convert pulled data to new data objects with desired properties
-    const newDataArray = data.map(({ name, address, profilePicture }) => {
-      let { city, state } = address ?? {};
-      const website = profilePicture;
-      state = stateAbbreviations[state];
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  // Use the following code when an API is known:
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-      return { name, city, state, website };
-    });
+  // const data = await fetchApiData();
+  // if (data) {
+  //   // Convert pulled data to new data objects with desired properties
+  //   const newDataArray = data.map(({ name, address, profilePicture }) => {
+  //     let { city, state } = address ?? {};
+  //     const website = profilePicture;
+  //     state = stateAbbreviations[state];
+  //     return { name, city, state, website };
+  //   });
+  //   // Set data size
+  //   apiData = newDataArray.filter((_, index) => index < 40);
+  //   currentDataSet = [...apiData];
+  //   // Render data
+  //   renderData(currentDataSet);
+  // }
 
-    // Set data size
-    apiData = newDataArray.filter((_, index) => index < 40);
-    currentDataSet = [...apiData];
-    // Render data
-    renderData(currentDataSet);
-  }
+  const newDataArray = mockData.map(({ name, city, state, website }) => {
+    return { name, city, state, website };
+  });
+
+  apiData = newDataArray.filter((_, index) => index < 31);
+  currentDataSet = [...apiData];
+  // Render data
+  renderData(currentDataSet);
 };
 
 init();

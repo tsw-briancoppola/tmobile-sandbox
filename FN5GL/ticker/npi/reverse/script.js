324c459d0a97be96d0d1f0f77678f8c982a346dd
Brian Coppola <BrianW.Coppola2@T-Mobile.com>
2026-07-24 10:54:39 -0700

    Added background image to FN5GL ticker

--- FN5GL/ticker/npi/reverse/script.js
+++ FN5GL/ticker/npi/reverse/script.js
@@ -8,9 +8,7 @@ const TICKER_REVERSE_2 = true; // false = forward, true = reverse
 // Init ticker 2
 // =-=-=-=-=-=-=
 
-const fn5glTickerContainer2 = document
-  .querySelector("#tsw-fn5gl-test_ticker-2")
-  .querySelector("xpr-npi-content").shadowRoot;
+const fn5glTickerContainer2 = document.querySelector("#tsw-fn5gl_ticker-2").querySelector("xpr-npi-content").shadowRoot;
 
 function waitForInitTicker(callback) {
   if (typeof initTicker !== "undefined") {
